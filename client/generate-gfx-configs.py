013cdcfee9197a6861181a09e85f903d05d0949a
Robert Maka <robert.maka@gmail.com>
2021-12-06 18:19:34 +0100

    cleanup, add generated gfx config files

--- client/generate-gfx-configs.py
+++ client/generate-gfx-configs.py
@@ -7,31 +7,24 @@ from json import load
 
 
 gfx_variables = {}
-
-
 target_order = ['pro', 'low', 'medium', 'high'] # interpolating from left to right
 
 with open("gfx-variables.json", "r") as f:
     gfx_variables = load(f)
 
-
-
 target_configs = {target: {} for target in target_order}
 
 # for target, config in target_configs.items():
 for variable, value_map in gfx_variables.items():
-    print(f'{variable} : {value_map}')
     last_target = None
     last_value = None
     for target in target_order:
         if target in value_map.keys():
-            print(f'found {target} in value_map')
             target_configs[target][variable] = value_map[target]
             last_target = target
             last_value = value_map[target]
 
         elif last_target in value_map.keys():
-            print(f'found last target {last_target} in value_map')
             target_configs[target][variable] = value_map[last_target]
         else:
             raise Error(f'No target ({target}) to interpolate from.')
